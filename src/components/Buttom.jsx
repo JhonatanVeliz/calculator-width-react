84c5fc042130da4ab9b7b8d9289823c1f9f909b6
Jhonatan Veliz <117142595+JhonatanVeliz@users.noreply.github.com>
2023-10-19 14:29:05 -0600

    se agrego responsive

--- src/components/Buttom.jsx
+++ src/components/Buttom.jsx
@@ -1,7 +1,7 @@
 const Buttom = ({ text = '', onclick = '', clase = '' }) => {
   return (
     <button onClick={onclick} className={ clase }>
-        { text }
+      { text }
     </button>
   )
 }
